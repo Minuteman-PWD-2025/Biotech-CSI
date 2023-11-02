7e630c825ad0178ee940a5af9aeec2f2c1ab7890
Ben Champagne <donkeykong797@gmail.com>
2023-11-02 13:25:29 -0400

    almost finished

--- src/util/Database.ts
+++ src/util/Database.ts
@@ -32,35 +32,46 @@ export class Row {
 }
 
 export default class Database {
-    requestUrl: string = "https://localhost:8080/api/";
+    static requestUrl: string = "https://localhost:8080/api/";
 
     // `GET/table=${tableName}`
     public static async getTable(token: string, tableName: string): Promise<DatabaseResponse<Row[]>> {
+        const response = await fetch(this.requestUrl.concat(`GET/token=${token}&table=${tableName}`));
+        const json = await response.json();
 
-        return {
-            error: 0,
-            message: "",
-            data: []
-        }
+        return new Promise<DatabaseResponse<Row[]>>(async resolve => {
+            resolve({
+                error: json.error,
+                message: json.message,
+                data: [...json.data]
+            })
+        })
     }
     // `Get/table=${tableName}&column=${columnName}&unique=${unique}`
     public static async getColumn(token: string, tableName: string, columnName: string, unique: boolean = false): Promise<DatabaseResponse<string[]>> {
+        const response = await fetch(this.requestUrl.concat(`GET/token=${token}&table=${tableName}&column=${columnName}&unique=${unique}`));
+        const json = await response.json();
 
-
-        return {
-            error: 0,
-            message: "",
-            data: []
-        }
+        return new Promise<DatabaseResponse<string[]>>(async resolve => {
+            resolve({
+                error: json.error,
+                message: json.message,
+                data: [...json.data]
+            })
+        })
     }
 
     // `POST/table=${tableName}&insert=${column,cell;column,cell}`
-    public static async postRow(tableName: string, row: Row): Promise<DatabaseResponse<void>> {
+    public static async postRow(token:string, tableName: string, row: Row): Promise<DatabaseResponse<void>> {
+        const response = await fetch(this.requestUrl.concat(`POST/token=${token}&table=${tableName}&insert=${row.data.forEach}`));
+        const json = await response.json();
 
-        return {
-            error: 0,
-            message: "",
-            data: void []
-        }
+        return new Promise<DatabaseResponse<void>>(async resolve => {
+            resolve({
+                error: json.error,
+                message: json.message,
+                data: void[]
+            })
+        })
     }
 }
