8e5545d698c03f50fc1eca83b746fdb740002f19
Ben Champagne <donkeykong797@gmail.com>
2023-11-02 14:00:23 -0400

    should be done

--- src/util/Database.ts
+++ src/util/Database.ts
@@ -30,10 +30,10 @@ export class Row {
         return this.data.get(columnName)
     }
 
-    public toString(): string {
+    public toString(removeNull: boolean = false): string {
         let pairs: string[] = [];
         this.data.forEach((value, key) => {
-            pairs.push(`${key},${value}`)
+            if (!(removeNull && !value)) pairs.push(`${key},${value}`)
         })
         return pairs.join(';')
     }
@@ -42,7 +42,7 @@ export class Row {
 export default class Database {
     static requestUrl: string = "https://localhost:8080/api/";
 
-    // `GET/table=${tableName}`
+    // `GET/token=${token}&table=${tableName}`
     // TODO: Implement Connection To Webserver
     public static async getTable(token: string, tableName: string): Promise<DatabaseResponse<Row[]>> {
         const response = await fetch(this.requestUrl.concat(`GET/token=${token}&table=${tableName}`));
@@ -56,8 +56,8 @@ export default class Database {
             })
         })
     }
-    
-    // `Get/table=${tableName}&column=${columnName}&unique=${unique}`
+
+    // `GET/token=${token}&table=${tableName}&column=${columnName}&unique=${unique}`
     // TODO: Implement Connection To Webserver
     public static async getColumn(token: string, tableName: string, columnName: string, unique: boolean = false): Promise<DatabaseResponse<string[]>> {
         const response = await fetch(this.requestUrl.concat(`GET/token=${token}&table=${tableName}&column=${columnName}&unique=${unique}`));
@@ -72,7 +72,7 @@ export default class Database {
         })
     }
 
-    // `POST/table=${tableName}&insert=${column,cell;column,cell}`
+    // `POST/token=${token}&table=${tableName}&insert=${column,cell;column,cell}`
     // TODO: Implement Connection To Webserver
     public static async postRow(token:string, tableName: string, row: Row): Promise<DatabaseResponse<void>> {
         const response = await fetch(this.requestUrl.concat(`POST/token=${token}&table=${tableName}&insert=${row.toString()}`));
@@ -83,6 +83,36 @@ export default class Database {
                 error: json.error,
                 message: json.message,
                 data: void[]
+            })
+        })
+    }
+
+    // `PUT/token=${token}&table=${tableName}&where=${column,cell;column,cell}&update=$(column,cell;column,cell)`
+    // TODO: Implement Connection To Webserver
+    public static async putRow(token:string, tableName: string, conditionalRow: Row, row: Row): Promise<DatabaseResponse<void>> {
+        const response = await fetch(this.requestUrl.concat(`PUT/token=${token}&table=${tableName}&where=${conditionalRow.toString()}&update=${row.toString(true)}`));
+        const json = await response.json();
+
+        return new Promise<DatabaseResponse<void>>(async resolve => {
+            resolve({
+                error: json.error,
+                message: json.message,
+                data: void[]
+            })
+        })
+    }
+
+    // `DELETE/token=${token}&table=${tableName}&where=${column,cell;column,cell}`
+    // TODO: Implement Connection To Webserver
+    public static async deleteRows(token: string, tableName: string, conditionalRow: Row): Promise<DatabaseResponse<Row[]>> {
+        const response = await fetch(this.requestUrl.concat(`DELETE/token=${token}&table=${tableName}&where=${conditionalRow.toString()}`));
+        const json = await response.json();
+
+        return new Promise<DatabaseResponse<Row[]>>(async resolve => {
+            resolve({
+                error: json.error,
+                message: json.message,
+                data: [...json.data]
             })
         })
     }
