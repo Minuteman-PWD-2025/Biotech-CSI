f073230661f0e8bfd71a2df5667115780d69292c
ihaveapatricklmao <82519853+ihaveapatricklmao@users.noreply.github.com>
2023-11-13 11:06:06 -0500

    init commit
    will add actual stuff to the page soon this is just a rough concept for how i will lay it out

---
+++ src/app/login/page.tsx
@@ -0,0 +1,35 @@
+﻿'use client';
+
+import { Button, Paper, TextField } from "@mui/material"
+import React from 'react';
+
+
+function Login() {
+    return (
+        <>
+            <TextField>
+                Username
+            </TextField>
+                Password
+            <TextField>
+            </TextField>
+            <Button>
+                Login
+            </Button>
+            <Button>
+                Forgot my password!
+            </Button>
+        </>
+    );
+}
+
+export default function Data() {
+
+    return (
+        <div className="tab">
+            <Paper>
+                <Login></Login>
+            </Paper>
+        </div>
+    );
+}
