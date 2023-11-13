b2a4e5784dade9110e1b6c8f2e0a1399256fd3c5
ihaveapatricklmao <82519853+ihaveapatricklmao@users.noreply.github.com>
2023-11-13 12:45:43 -0500

    made functions that will be eventually completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,23 +4,29 @@ import { Button, Paper, TextField } from "@mui/material"
 import React from 'react';
 
 
+function SendData({string} : id) {
+    // put actual stuff to look at user data here
+}
+
+function ForgotPass() {
+    // put actual forgot password stuff here
+}
+
 function Login() {
     return (
         <>
             <TextField id="username" label="Username" variant="outlined" />
             <TextField id="password" label="Password" variant="outlined" />
-            <Button id="login" size="small" >
+            <Button id="login" size="small" onClick={ () => SendData('login')}>
                 Login
             </Button>
-            <Button id="forgot-pass" size="small" >
+            <Button id="forgot-pass" size="small" onClick={ () => ForgotPass()}>
                 Forgot
             </Button>
         </>
     );
 }
 
-function SendData() {
-}
 
 export default function Data() {
 
