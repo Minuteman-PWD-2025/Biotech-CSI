f9e722ab926013c59eb3cdcfb27729c895e1b619
Ben Champagne <donkeykong797@gmail.com>
2023-11-30 09:10:53 -0500

    should be done now

--- src/app/materials/page.tsx
+++ src/app/materials/page.tsx
@@ -1,70 +1,17 @@
-'use client';
+import Table from '@/components/Table';
+import Database, { Row } from '@/util/Database';
+import React from 'react';
 
-import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
-import React, { useMemo } from 'react';
-
-export default function Data() {
-    const data = [
-        {
-            id: 1,
-            item: "Dangerous Chemical",
-        },
-        {
-            id: 2,
-            item: "Chemical X",
-        },
-        {
-            id: 3,
-            item: "Chemical ???",
-        },
-        {
-            id: 4,
-            item: "Kryptonite",
-        },
-        {
-            id: 5,
-            item: "Dog Cells",
-        },
-        {
-            id: 6,
-            item: "Cells of Mr. Pig",
-        },
-        {
-            id: 7,
-            item: "Mr. Steven's Growing Tissue",
-        },
-    ]
-
-    const columns = useMemo(
-        () => [
-            {
-                accessorKey: 'id',
-                header: 'Id',
-            },
-            {
-                accessorKey: 'item',
-                header: 'Item',
-            },
-        ],
-        [],
-    );
-
-    const table = useMaterialReactTable({
-        columns,
-        data,
-        enablePagination: false, //disable a default feature
-        enableBottomToolbar: false,
-        enableSorting: true,
-        columnFilterModeOptions: null,
-        enableFullScreenToggle: false,
-        muiTablePaperProps: {
-            elevation: 0,
-        },
-    });
+export default function Data({ token = "token" }: { token: string }) {
+    let data: Row[] = []
+    Database.getTable(token, "items").then((res) => {
+        data = res.data
+        console.log(res)
+    })
 
     return (
         <div className="tab">
-            <MaterialReactTable table={table}/> 
+            <Table data={data} />
         </div>
     );
 }
--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,70 +1,17 @@
-'use client';
+import Table from '@/components/Table';
+import Database, { Row } from '@/util/Database';
+import React from 'react';
 
-import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
-import React, { useMemo } from 'react';
-
-export default function Overview() {
-    const data = [
-        {
-            checked_out: "Dangerous Chemical, Chemical X, Chemical ???",
-            person: "Mr. Llama",
-        },
-        {
-            checked_out: "",
-            person: "Mr. Pidgeon",
-        },
-        {
-            checked_out: "Dog Cells",
-            person: "Ms. Eagle",
-        },
-        {
-            checked_out: "Kryptonite",
-            person: "John",
-        },
-        {
-            checked_out: "Mr. Steven's Growing Tissue, Cells of Mr. Pig",
-            person: "Ms. Fish",
-        },
-        {
-            checked_out: "",
-            person: "Mr. Pig",
-        },
-        {
-            checked_out: "",
-            person: "Mr. Steven",
-        },
-    ]
-
-    const columns = useMemo(
-        () => [
-            {
-                accessorKey: 'person',
-                header: 'Person',
-            },
-            {
-                accessorKey: 'checked_out',
-                header: 'Checked Out Materials',
-            },
-        ],
-        [],
-    );
-
-    const table = useMaterialReactTable({
-        columns,
-        data,
-        enablePagination: false, //disable a default feature
-        enableBottomToolbar: false,
-        enableSorting: true,
-        columnFilterModeOptions: null,
-        enableFullScreenToggle: false,
-        muiTablePaperProps: {
-            elevation: 0,
-        },
-    });
+export default function Data({ token = "token" }: { token: string }) {
+    let data: Row[] = []
+    Database.getTable(token, "people_items").then((res) => {
+        data = res.data
+        console.log(res)
+    })
 
     return (
         <div className="tab">
-            <MaterialReactTable table={table}/> 
+            <Table data={data} />
         </div>
     );
 }
