72dc540b225e8f4d8c40b58f2956e74a2fc7f743
Ben Champagne <donkeykong797@gmail.com>
2023-12-13 11:28:51 -0500

    fixed

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -3,7 +3,7 @@ import Database, { Row } from '@/util/Database';
 import { getCookie } from 'cookies-next';
 import React from 'react';
 
-export default async function Data({ token }: { token: string }) {
+export default async function Data({ token = "..."}: { token: string }) {
     let data: Row[] = (await Database.getTable(token, "people_items")).data
 
     return (
